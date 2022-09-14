71ef71882a2707a963fce2760dbb4bf943f009bd
Ian Jeffers <ianjeffers@Ians-Air.attlocal.net>
2022-09-14 14:41:11 -0700

    Adding Buttons, logging

--- src/Display.py
+++ src/Display.py
@@ -1,15 +1,15 @@
 import tkinter as tk
 import cv2
-import numpy as np
 from PIL import ImageTk, Image
 from Detector import Detector
-
+import logging
 
 class Display():
     def __init__(self):
         self.initialize_variables()
         self.create_window()
         self.create_label()
+        self.create_buttons()
         self.show_frames()
         self.root.mainloop()
 
@@ -24,15 +24,18 @@ class Display():
         self.root.geometry(str(self.width) + "x" + str(self.height))
         self.root.title("Webcam")
 
+    def create_buttons(self):
+        btn = tk.Button(self.root, text='Disable AI', bd='5', command=self.disable_ai).grid(row=6, column=0)
+        btn = tk.Button(self.root, text='Enable AI', bd='5', command=self.enable_ai).grid(row=5, column=0)
+
+
     def create_label(self):
         self.label = tk.Label(self.root)
         self.label.grid(row=0, column=0)
         self.cap = cv2.VideoCapture(0)
 
     def show_frames(self):
-        # Get the latest frame and convert into Image
         cv2image = cv2.cvtColor(self.cap.read()[1], cv2.COLOR_BGR2RGB)
-
         if(self.ai):
             img = Image.fromarray(self.detector.get_prediction(cv2image))
         else:
@@ -45,7 +48,9 @@ class Display():
         self.label.after(1, self.show_frames)
 
     def enable_ai(self):
+        logging.info("Enabling AI")
         self.ai = True
 
     def disable_ai(self):
+        logging.info("Disabling AI")
         self.ai = False
--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from Display import Display
+import logging
 
 if __name__ == '__main__':
+    logging.info("Launching App")
     screen = Display()
+    logging.info("Display Launched")
